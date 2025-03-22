f5a85e2e17ad61d5a994e61d267b3fa9f80aef5f
Adalberto Deluca <adalbertodeluca@MacBook-Pro-de-Adalberto.local>
2025-03-22 15:22:49 -0300

    Preparando para rodar com Gunicorn

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ import os
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return 'Bot NPS rodando com sucesso no Render!'
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     dados = request.json
-    
-    # Logs para verificar que o webhook foi acionado
     print("✅ Webhook acionado!")
     print(f"📩 Dados recebidos: {dados}")
 
     return jsonify({"status": "mensagem recebida!"})
-    
+
