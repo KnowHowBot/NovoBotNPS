7412130b0119f22dfd6a9c150b78133ebb08e475
Adalberto Deluca <adalbertodeluca@MacBook-Pro-de-Adalberto.local>
2025-03-22 16:21:44 -0300

    Corrigido: Adicionando aspas em instancia e token

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ import requests
 
 app = Flask(__name__)
 
-# Dados da sua instância Z-API
 INSTANCIA = “3DE8910478AF3063BBAB32C54B267657”
 TOKEN = “4C238699A42CC1F7AC28584D
 
-# Função para enviar mensagem automática via Z-API
 def enviar_mensagem(numero, mensagem):
 	https://api.z-api.io/instances/3DE8910478AF3063BBAB32C54B267657/token/4C238699A42CC1F7AC28584D/send-text
     payload = {
@@ -31,15 +29,12 @@ def webhook():
     print("✅ Webhook acionado!")
     print(f"📩 Dados recebidos: {dados}")
 
-    # Extrair número e mensagem recebida
     numero = dados.get('phone')   # número do cliente
     mensagem = dados.get('message')  # mensagem recebida
 
-    # Aqui vai a lógica simples de resposta
     if mensagem:
         resposta = f"Obrigado pelo seu feedback: {mensagem}!"
         
-        # Enviar resposta automática
         enviar_mensagem(numero, resposta)
 
     return jsonify({"status": "mensagem recebida e respondida!"})
